b3ade38f0fd5dbaf7c432e0eb12744d650070dbe
Duken Marga <dukenmarga@gmail.com>
2023-04-15 06:56:01 +0700

    Create text input to place the long url

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<section class="section is-medium">
+    <h2 class="has-text-danger">URL Short!</h2>
+    <h6>by @github/dukenmarga</h6>
+    <div class="field">
+        <div class="control">
+            <label for="url" class="has-text-weight-bold">Your URL</label>
+            <input id="url" class="input" type="text" placeholder="Your long url here ex. https://yourlongurl.com/many-words-in-the-path-that-you-need-to-make-it-shorter">
+        </div>
+    </div>
+    <div class="field">
+        <div class="control">
+            <button class="button is-info">Make it short!</button>
+        </div>
+    </div>
+</section>
